e7bedf43a26d13c673b24f633d8b5c93578e6915
Lethaha <lethha.thamae@gmail.com>
2022-06-12 08:12:39 +0200

    employee manager first back end commit

---
+++ src/main/java/com/lethaha/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.lethaha.employeemanager.repo;
+
+import com.lethaha.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long Id);
+}
